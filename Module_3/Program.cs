337a28254ebbf184baf9c9e8c3b1f5fc92d0d1e7
Sergio Ebanatti <sergioebanatti@gmail.com>
2022-11-30 13:58:33 +0700

    3.1 complete

--- Module_3/Program.cs
+++ Module_3/Program.cs
@@ -1,15 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Module_3
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static string userString;
+        private static int userNumber;
+
+        static void Main()
         {
+            UserStringInput();
+
+            if (IsUserNumberEven())
+            {
+                Console.WriteLine("Введённое число является чётным");
+            }
+            else 
+            {
+                Console.WriteLine("Введённое число является нечётным");
+            }
+
+            userString = Console.ReadLine();
         }
+
+        static void UserStringInput()
+        {
+            Console.WriteLine("Введите целое число");
+            userString = Console.ReadLine();
+            UserStringCheck();
+        }
+
+        static void UserStringCheck()
+        {
+            if (int.TryParse(userString, out int result))
+            {
+                userNumber = result;
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели не корректные данные!");
+                UserStringInput();
+            }
+        }
+
+        static bool IsUserNumberEven() 
+        {
+            if (userNumber%2==0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
     }
 }
